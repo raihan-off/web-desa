2909a7eed68f44d23d91ba1a837ec768fddf1c6d
Jannatin Nurrohmah <jannatinnurrohmah@gmail.com>
2021-02-01 08:07:46 +0700

    Galeri & Berita

--- src/views/Berita.vue
+++ src/views/Berita.vue
@@ -1,5 +1,231 @@
 <template>
-  <div class="berita">
-    <h1>This is an berita page</h1>
+  <div>
+    <div style="text-align: center;" class="mt-16 mb-16">
+      <h2>Berita Terkini</h2>
+      quis ullamco officia adipisicing excepteur do ad excepteur.
+    </div>
+    <v-col>
+      <v-row class="ml-16 mr-16 mb-12">
+        <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="@/assets/galeri/foto1.png"
+            height="200px"
+          ></v-img>
+
+          <v-card-subtitle>
+            Tanggalnya
+          </v-card-subtitle>
+
+          <v-card-title>
+            Top western road trips
+          </v-card-title>
+
+          <v-card-text class="text--primary">
+            <div>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. Vitae dolor at quis id dignissim quis. 
+              Vel, pharetra vestibulum eu non integer semper purus tincidunt. 
+              Hendrerit aliquam tempor nibh id faucibus semper.</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              color="red darken-2"
+              text
+              @click="show = !show"
+            >
+              Read more
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="@/assets/galeri/foto2.png"
+            height="200px"
+          ></v-img>
+
+          <v-card-subtitle>
+            Tanggalnya
+          </v-card-subtitle>
+
+          <v-card-title>
+            Top western road trips
+          </v-card-title>
+
+          <v-card-text class="text--primary">
+            <div>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. Vitae dolor at quis id dignissim quis. 
+              Vel, pharetra vestibulum eu non integer semper purus tincidunt. 
+              Hendrerit aliquam tempor nibh id faucibus semper.</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              color="red darken-2"
+              text
+              @click="show = !show"
+            >
+              Read more
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="@/assets/galeri/foto3.png"
+            height="200px"
+          ></v-img>
+
+          <v-card-subtitle>
+            Tanggalnya
+          </v-card-subtitle>
+
+          <v-card-title>
+            Top western road trips
+          </v-card-title>
+
+          <v-card-text class="text--primary">
+            <div>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. Vitae dolor at quis id dignissim quis. 
+              Vel, pharetra vestibulum eu non integer semper purus tincidunt. 
+              Hendrerit aliquam tempor nibh id faucibus semper.</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              color="red darken-2"
+              text
+              @click="show = !show"
+            >
+              Read more
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-row>
+
+      <v-row class="ml-16 mr-16 mb-16">
+        <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="@/assets/galeri/foto4.png"
+            height="200px"
+          ></v-img>
+
+          <v-card-subtitle>
+            Tanggalnya
+          </v-card-subtitle>
+
+          <v-card-title>
+            Top western road trips
+          </v-card-title>
+
+          <v-card-text class="text--primary">
+            <div>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. Vitae dolor at quis id dignissim quis. 
+              Vel, pharetra vestibulum eu non integer semper purus tincidunt. 
+              Hendrerit aliquam tempor nibh id faucibus semper.</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              color="red darken-2"
+              text
+              @click="show = !show"
+            >
+              Read more
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="@/assets/galeri/foto5.png"
+            height="200px"
+          ></v-img>
+
+          <v-card-subtitle>
+            Tanggalnya
+          </v-card-subtitle>
+
+          <v-card-title>
+            Top western road trips
+          </v-card-title>
+
+          <v-card-text class="text--primary">
+            <div>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. Vitae dolor at quis id dignissim quis. 
+              Vel, pharetra vestibulum eu non integer semper purus tincidunt. 
+              Hendrerit aliquam tempor nibh id faucibus semper.</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              color="red darken-2"
+              text
+              @click="show = !show"
+            >
+              Read more
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-card
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-img
+            src="@/assets/galeri/foto6.png"
+            height="200px"
+          ></v-img>
+
+          <v-card-subtitle>
+            Tanggalnya
+          </v-card-subtitle>
+
+          <v-card-title>
+            Top western road trips
+          </v-card-title>
+
+          <v-card-text class="text--primary">
+            <div>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. Vitae dolor at quis id dignissim quis. 
+              Vel, pharetra vestibulum eu non integer semper purus tincidunt. 
+              Hendrerit aliquam tempor nibh id faucibus semper.</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              color="red darken-2"
+              text
+              @click="show = !show"
+            >
+              Read more
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-row>
+    </v-col>
   </div>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      show: false,
+    }),
+  }
+</script>
--- src/views/Galeri.vue
+++ src/views/Galeri.vue
@@ -1,5 +1,77 @@
 <template>
-  <div class="galeri">
-    <h1>This is an galeri page</h1>
+  <div style="position: center;">
+    <div style="text-align: center;" class="mt-16 mb-16">
+      <h2>Galeri</h2>
+      quis ullamco officia adipisicing excepteur do ad excepteur.
+    </div>
+    <v-col class="mb-16 ml-16 mr-16">
+      <v-row class="mb-8">
+        <v-img
+          src="@/assets/galeri/foto1.png"
+          max-height="400"
+          max-width="712"
+        ></v-img>
+        <v-col style="padding:0px;" class="ml-6">
+          <v-img
+            class="mb-6"
+            src="@/assets/galeri/foto2.png"
+            max-height="185"
+            max-width="344"
+          ></v-img>
+          <v-img
+            class=""
+            src="@/assets/galeri/foto3.png"
+            max-height="190"
+            max-width="344"
+          ></v-img>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-img
+          class="mr-6"
+          src="@/assets/galeri/foto4.png"
+          max-height="400"
+          max-width="344">
+        </v-img>
+        <v-img
+          class="mr-6"
+          src="@/assets/galeri/foto5.png"
+          max-height="400"
+          max-width="344"
+        ></v-img>
+        <v-img
+          class="mr-6"
+          src="@/assets/galeri/foto6.png"
+          max-height="400"
+          max-width="344"
+        ></v-img>
+      </v-row>
+      <v-row class="mt-8">
+        <v-img
+          class="mr-6"
+          src="@/assets/galeri/foto8.png"
+          max-height="400"
+          max-width="344">
+        </v-img>
+        <v-img
+          src="@/assets/galeri/foto7.png"
+          max-height="400"
+          max-width="712">
+        </v-img>
+      </v-row>
+      <v-row class="mt-8">
+      <v-img
+        class="mr-6"
+        src="@/assets/galeri/foto9.png"
+        max-height="400"
+        max-width="712"
+      ></v-img>
+      <v-img
+        src="@/assets/galeri/foto10.png"
+        max-height="400"
+        max-width="344"
+      ></v-img>
+      </v-row>
+    </v-col>
   </div>
 </template>
