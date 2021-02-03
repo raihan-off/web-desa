8fbbe8be8f2242faa4b7e71739c83f78942048a8
raihan-off <raihan.official230@gmail.com>
2021-02-03 14:13:28 +0700

    landing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,44 @@
 <template>
   <div class="home">
-    <div class="jumbotron text--white text-center pa-16">
-      <h3 class="white--text">
+    <div class="jumbotron text--white text-center">
+      <h1 class="white--text">
         Selamat Datang di Website<br />
         Resmi Desa Sukamaju
-      </h3>
+      </h1>
       <v-btn class="white--text mt-10" color="red">More</v-btn>
     </div>
     <div class="data-penduduk">
       <h3>Data Penduduk</h3>
       <h4>Berikut Merupakan Perkiraan Data Penduduk</h4>
-      <div class="row justify-center mt-10">
-        <div class="penduduk">
-          <h5>{{ datapenduduk }}</h5>
+      <div>
+        <div class="penduduk mt-15">
+          <v-list class="stats align-center">
+            <v-list-tile
+              class="detail-stats"
+              v-for="penduduk in datapenduduk"
+              :key="penduduk.text"
+            >
+              <div>
+                {{ penduduk.jumlah }} <br />
+                {{ penduduk.name }}
+              </div>
+            </v-list-tile>
+          </v-list>
         </div>
+      </div>
+    </div>
+    <div class="quote pa-16">
+      <div class="pemimpin"></div>
+      <div class="ucapan">
+        <h3 class="text--white">
+          Selamat datang di Website resmi Pemerintah Desa Sukamaju Kecamatan
+          Belik Kabupaten Pemalang Provinsi Jawa Tengah. Ini adalah ruang media
+          informasi desa sebagai sarana komunikasi dan keterbukaan informasi
+          publik. Jangan lupa selalu ikuti website dan media sosial kami untuk
+          update informasi dalam penyelenggaran pemerintahan di Desa Sukamaju.
+          Terima kasih sudah mengunjungi website kami. Semoga bermanfaat, kritik
+          dan saran selalu kami harapkan untuk desa yang lebih baik
+        </h3>
       </div>
     </div>
   </div>
@@ -67,6 +92,7 @@ export default {
   background-image: url("../assets/jumbotron.png");
   background-repeat: no-repeat;
   background-size: 100%;
+  padding: 200px 0 200px 0;
 }
 
 .data-penduduk {
@@ -75,14 +101,22 @@ export default {
 }
 
 .penduduk {
-  padding: 5%;
+  padding: 3%;
+}
+
+.stats {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.detail-stats {
+  margin: 4%;
 }
 
 .quote {
-  background-image: url("../assets/kd-image.png");
+  background-image: url("../assets/back-quote.png");
   background-repeat: no-repeat;
   background-size: 100%;
-  display: flex;
-  flex-direction: row;
 }
 </style>
